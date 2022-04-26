6415f2290f9b9dbd64fa9de185f8e38e0b9e797b
hoangtrinh666 <trinhhoang159357@gmail.com>
2022-04-26 15:31:37 +0700

    updatess

---
+++ js/typewriter-custom.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/* ========= Typewriter effect =========== */
+/* Ref: https://safi.me.uk/typewriterjs/ */
+const instance = new Typewriter('#typewriter', {
+    strings: ['Sự Chuyên Nghiệp', 'Công Nghệ Mới', 'Tiết Kiệm Chi Phí', 'Thân Thiện Người Dùng','Tiến Độ Công Việc','Chất Lượng'],
+    autoStart: true,
+    loop: true,
+});
